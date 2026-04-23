6ef4b754efcac4e477d3ad650e1fe29cc34549ba
Barry Prendergast <barry.m.prendergast@gmail.com>
2026-04-23 12:53:11 +0200

    feat: add SectionPitch iframe embed component (#44)

---
+++ src/components/SectionPitch/SectionPitch.styles.ts
@@ -0,0 +1,9 @@
+export const gridWrapper =
+  'grid grid-cols-3 2xl:grid-cols-4 gap-x-32 gap-y-32 w-full';
+
+export const colStyles =
+  'col-span-3 lg:col-span-2 flex flex-col gap-16 items-start justify-self-stretch self-start';
+
+export const aspectWrapper = 'relative w-full aspect-video';
+
+export const iframeStyles = 'absolute inset-0 w-full h-full border-0';
